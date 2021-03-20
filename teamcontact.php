f74d0f0df0ebc1bc6dca05b0bf21cb97290bc927
Suraj Rohankar <39958336+surajrohankar@users.noreply.github.com>
2021-03-20 22:51:15 +0530

    email updated
    email updated

--- teamcontact.php
+++ teamcontact.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "contact@engitect.com";
+    $email_to = "suraj@eliteworklab.com";
     $email_subject = "Your email subject line";
  
     function died($error) {
